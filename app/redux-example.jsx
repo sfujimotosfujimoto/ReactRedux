4fd841713692da617e47ee05aa8f118773fd3566
sfujimoto <unicoshun@gmail.com>
2016-09-15 22:10:45 +0900

    ~#114 add support for location fetching

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,62 +1,19 @@
 import redux, {createStore, subscribe, compose, combineReducers} from 'redux';
-
+import axios from 'axios';
 console.log('Starting redux example');
 
 //-- DEFAULT STATE ------
 let stateDefault = {
   name: 'Anonymous',
   hobbies: [],
-  movies: []
+  movies: [],
+  isFetching: false,
+  url: ''
 }
 let nextHobbyId = 1;
 let nextMovieId = 1;
 
 //-- REDUCER ------
-const oldreducer = (state = stateDefault, action) => {
-  // state = state || {name: 'Anonymous'};
-  switch (action.type) {
-    case 'CHANGE_NAME':
-      return {
-        ...state,
-        name: action.name
-      }
-    case 'ADD_HOBBY':
-      return {
-        ...state,
-        hobbies: [
-          ...state.hobbies,
-          {
-            id: nextHobbyId++,
-            hobby: action.hobby
-          }
-        ]
-      }
-    case 'REMOVE_HOBBY':
-      return {
-        ...state,
-        hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
-      }
-    case 'ADD_MOVIE':
-      return {
-        ...state,
-        movies: [
-          ...state.movies,
-          {
-            id: nextMovieId++,
-            title: action.title,
-            genre: action.genre
-          }
-        ]
-      }
-    case 'REMOVE_MOVIE':
-      return {
-        ...state,
-        movies: state.movies.filter((movie) => movie.id !== action.id)
-      }
-    default:
-      return state;
-  }
-};
 
 let nameReducer = (state = 'Anonymous', action) => {
   switch (action.type) {
@@ -141,10 +98,54 @@ let removeMovie = (id) => {
   }
 }
 
+//-- Map Reducer and Action Generators ------
+const mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+  switch (action.type) {
+    case 'START_LOCATION_FETCH':
+      return {
+        isFetching: true,
+        url: undefined
+      }
+    case 'COMPLETE_LOCATION_FETCH':
+      return {
+        isFetching: false,
+        url: action.url
+      }
+    default:
+      return state;
+  }
+}
+
+//-- Action Generator ------
+let startLocationFetch = () => {
+  return {
+    type: 'START_LOCATION_FETCH'
+  }
+}
+
+let completeLocationFetch = (url) => {
+  return {
+    type: 'COMPLETE_LOCATION_FETCH',
+    url
+  }
+}
+
+let fetchLocation = () => {
+  store.dispatch(startLocationFetch());
+
+  axios.get('http://ipinfo.io').then(function (res) {
+    let loc = res.data.loc;
+    let baseUrl = 'http://maps.google.com?q=';
+
+    store.dispatch(completeLocationFetch(baseUrl + loc));
+  })
+}
+
 const reducer = combineReducers({
   name: nameReducer,
   hobbies: hobbiesReducer,
-  movies: moviesReducer
+  movies: moviesReducer,
+  map: mapReducer
 })
 
 
@@ -158,16 +159,22 @@ const store = createStore(reducer, compose(
 let unsubscribe = store.subscribe(() => {
   let state = store.getState();
 
-  console.log('Name is', state.name);
-  document.getElementById('app').innerHTML = state.name;
 
   console.log('New state', store.getState());
+
+  if (state.map.isFetching) {
+    document.getElementById('app').innerHTML = 'Loading...';
+  } else if (state.map.url) {
+    document.getElementById('app').innerHTML = '<a href="' + state.map.url +  '" target="_blank">View Your Location</a>'
+  }
 })
 // unsubscribe();
 
 
 const currentState = store.getState();
 console.log('currentState', currentState);
+
+fetchLocation();
 
 store.dispatch(changeName('Andrew'));
 
